fd28fbc51ff8afdb3f30f0c4ab62cdc274901127
Anand <anands7957@gmail.com>
2021-12-26 01:05:50 +0530

    added big-tiitle, workpage animation

--- src/App.js
+++ src/App.js
@@ -2,26 +2,33 @@ import "./App.css";
 import GlobalStyle from "./globalStyles";
 import { ThemeProvider } from "styled-components";
 import { lightTheme } from "./components/Themes";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import MyWork from "./components/MyWork";
 import BlogPage from "./components/BlogPage";
 import MySkillsPage from "./components/MySkillsPage";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  
+  // TODO -> learn useLocation
+  const location = useLocation();
+
   return (
     <ThemeProvider theme={lightTheme}>
-      
-        <GlobalStyle />
-        {/* <h1>Hello World</h1> */}
-        <Routes>
+      <GlobalStyle />
+      {/* <h1>Hello World</h1> */}
+      {/* For Framer-motion animation on page change*/}
+      <AnimatePresence exitBeforeEnter>
+        <Routes location={location} key={location.pathname}>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
           <Route exact path="/mywork" element={<MyWork />} />
           <Route exact path="/blogs" element={<BlogPage />} />
           <Route exact path="/skills" element={<MySkillsPage />} />
         </Routes>
+      </AnimatePresence> 
     </ThemeProvider>
   );
 }
