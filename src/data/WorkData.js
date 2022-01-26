b11d53102d53783e579e4a8ebcc76c72ecc5c523
Anand <anands7957@gmail.com>
2022-01-26 11:35:03 +0530

    fix

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -45,17 +45,5 @@ export const Work = [
         demo:"https://heuristic-goodall-c2f0cc.netlify.app/",
         github:"https://github.com/codebucks27/React-Redux-Todo-App"
         
-    },
-    {
-        id:6,
-        name:"Responsive Navigation Menu",
-        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
-        tags:["react","react-router","css"],
-        
-        demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-responsive-navbar"
-        
-    },
-    
-  
+    }
 ] 
